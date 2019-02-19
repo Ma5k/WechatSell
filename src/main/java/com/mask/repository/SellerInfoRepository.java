d47142dfb0cd2caa784814df246f991506e823fd
Mask <2421697126@qq.com>
2019-02-19 23:44:34 +0800

    2019-02-19
    登录成功

---
+++ src/main/java/com/mask/repository/SellerInfoRepository.java
@@ -0,0 +1,8 @@
+package com.mask.repository;
+
+import com.mask.dataobject.SellerInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerInfoRepository extends JpaRepository<SellerInfo, String> {
+    SellerInfo findByOpenid(String openid);
+}
---
+++ src/main/java/com/mask/service/impl/SellerServiceImpl.java
@@ -0,0 +1,20 @@
+package com.mask.service.impl;
+
+import com.mask.dataobject.SellerInfo;
+import com.mask.repository.SellerInfoRepository;
+import com.mask.service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SellerServiceImpl implements SellerService {
+
+    @Autowired
+    private SellerInfoRepository repository;
+
+    @Override
+    public SellerInfo findSellerInfoByOpenid(String openid) {
+        return repository.findByOpenid(openid);
+    }
+}
+
